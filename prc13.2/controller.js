f2b448c280713c268f53e7ff8fce7274347cbc3e
nilakshisonmale <nilakshisonmale557@gmail.com>
2026-02-05 05:23:13 +0000

    .

--- prc13.2/controller.js
+++ prc13.2/controller.js
@@ -1,45 +1,11 @@
-<%@ page contentType="text/html" %>
-<html>
-<head>
-    <title>JSP Controller Example</title>
-</head>
-<body>
+var app = angular.module("myApp", []);
 
-<h2>1. Basic Controller</h2>
-<%
-    String message = "Hello from Basic Controller";
+app.controller("StudentCtrl", function ($scope) {
 
-    void greet(JspWriter out) throws Exception {
-        out.println("Welcome!");
-    }
-%>
-<p><%= message %></p>
+    $scope.students = [
+        { name: "Rahul", rollno: 103, marks: 78 },
+        { name: "Vedant", rollno: 101, marks: 85 },
+        { name: "Amit", rollno: 102, marks: 90 }
+    ];
 
-<hr>
-
-<h2>2. Parent Controller</h2>
-<%
-    String parentData = "This data is from Parent Controller";
-%>
-<p><%= parentData %></p>
-
-<hr>
-
-<h2>3. Child Controller</h2>
-<%
-    String childData = "This data is from Child Controller";
-%>
-<p><%= childData %></p>
-
-<hr>
-
-<h2>4. Filter Controller</h2>
-<%
-    String name = "client side scripting";
-    int fees = 2500;
-%>
-<p>Name: <%= name.toUpperCase() %></p>
-<p>Fees: <%= fees %></p>
-
-</body>
-</html>
+});
